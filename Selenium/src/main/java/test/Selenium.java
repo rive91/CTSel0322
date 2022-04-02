758dd0a99ffdcb22dbeb6661166ea9493c973634
Marcial Rivera <mrivera@pfl.com>
2022-04-01 19:20:39 -0600

    new branch

--- Selenium/src/main/java/test/Selenium.java
+++ Selenium/src/main/java/test/Selenium.java
@@ -9,6 +9,7 @@ public class Selenium {
 		String exePath = "./driver/chromedriver.exe"; //especificar la ruta del ChromeDriver
 		System.setProperty("webdriver.chrome.driver", exePath); //Agregando la propepiedas con la ruta del chrome driver
 		WebDriver driver = new ChromeDriver(); //Objeto de webDriver
+		
 		driver.get("https://www.google.com");
 		
 	}
